5051d34b08bca95ae283594a32a9b160ef798184
Luka Kacil <luka.kacil@gmail.com>
2018-06-06 21:04:32 +0200

    Make Zxc inherit from ERC20 Token implementation

--- contracts/tokens/Zxc.sol
+++ contracts/tokens/Zxc.sol
@@ -9,68 +9,13 @@ import "@0xcert/ethereum-erc20/contracts/tokens/Token.sol";
  * @dev Standard ERC20 token used by the protocol. This contract follows the
  * implementation at https://goo.gl/64yCkF.
  */
-contract Zxc is Ownable {
+contract Zxc is Token, Ownable {
   using SafeMath for uint256;
-
-  /**
-   * Token name.
-   */
-  string internal tokenName;
-
-  /**
-   * Token symbol.
-   */
-  string internal tokenSymbol;
-
-  /**
-   * Number of decimals.
-   */
-  uint8 internal tokenDecimals;
-
-  /**
-   * Total supply of tokens.
-   */
-  uint256 internal tokenTotalSupply;
-
-  /**
-   * Balance information map.
-   */
-  mapping (address => uint256) internal balances;
-
-  /**
-   * Token allowance mapping.
-   */
-  mapping (address => mapping (address => uint256)) internal allowed;
 
   /**
    * Transfer feature state.
    */
   bool internal transferEnabled;
-
-  /**
-   * @dev An event which is triggered when funds are transfered.
-   * @param _from The address sending tokens.
-   * @param _to The address recieving tokens.
-   * @param _value The amount of transferred tokens.
-   */
-  event Transfer(
-    address indexed _from,
-    address indexed _to,
-    uint256 _value
-  );
-
-  /**
-   * @dev An event which is triggered when an address to spend the specified amount of
-   * tokens on behalf is approved.
-   * @param _owner The address of an owner.
-   * @param _spender The address which spent the funds.
-   * @param _value The amount of spent tokens.
-   */
-  event Approval(
-    address indexed _owner,
-    address indexed _spender,
-    uint256 _value
-  );
 
   /**
    * @dev An event which is triggered when tokens are burned.
@@ -117,62 +62,6 @@ contract Zxc is Ownable {
   }
 
   /**
-   * @dev Returns the name of the token.
-   */
-  function name()
-    external
-    view
-    returns (string _name)
-  {
-    _name = tokenName;
-  }
-
-  /**
-   * @dev Returns the symbol of the token.
-   */
-  function symbol()
-    external
-    view
-    returns (string _symbol)
-  {
-    _symbol = tokenSymbol;
-  }
-
-  /**
-   * @dev Returns the number of decimals the token uses.
-   */
-  function decimals()
-    external
-    view
-    returns (uint8 _decimals)
-  {
-    _decimals = tokenDecimals;
-  }
-
-  /**
-   * @dev Returns the total token supply.
-   */
-  function totalSupply()
-    external
-    view
-    returns (uint256 _totalSupply)
-  {
-    _totalSupply = tokenTotalSupply;
-  }
-
-  /**
-   * @dev Returns the account balance of another account with address _owner.
-   * @param _owner The address from which the balance will be retrieved.
-   */
-  function balanceOf(address _owner)
-    external
-    view
-    returns (uint256 _balance)
-  {
-    _balance = balances[_owner];
-  }
-
-  /**
    * @dev transfer token for a specified address
    * @param _to The address to transfer to.
    * @param _value The amount to be transferred.
@@ -213,42 +102,6 @@ contract Zxc is Ownable {
 
     emit Transfer(_from, _to, _value);
     return true;
-  }
-
-  /**
-   * @dev Approves the passed address to spend the specified amount of tokens on behalf
-   * of the msg.sender. This function is based on StandardToken implementation at goo.gl/GZEhaq
-   * and goo.gl/fG8R4i.
-   * To change the approve amount you first have to reduce the spender's allowance to zero by
-   * calling `approve(_spender, 0)` if it is not already 0 to mitigate the race condition described
-   * here https://goo.gl/7n9A4J.
-   * @param _spender The address which will spend the funds.
-   * @param _value The allowed amount of tokens to be spent.
-   */
-  function approve(address _spender, uint256 _value)
-    public
-    returns (bool)
-  {
-    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-
-    allowed[msg.sender][_spender] = _value;
-
-    emit Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  /**
-   * @dev Returns the amount of tokens that a spender can transfer on behalf of an owner. This
-   * function is based on StandardToken implementation at goo.gl/GZEhaq.
-   * @param _owner The address which owns the funds.
-   * @param _spender The address which will spend the funds.
-   */
-  function allowance(address _owner, address _spender)
-    public
-    view
-    returns (uint256)
-  {
-    return allowed[_owner][_spender];
   }
 
   /**
--- test/tokenks/Xct.test.js
+++ test/tokenks/Xct.test.js
@@ -14,40 +14,6 @@ contract('erc/Zxc', (accounts) => {
     token = await Zxc.new();
   });
 
-  it('has correct tokenSupply after construction', async () => {
-    const actualSupply = await token.totalSupply();
-    assert.equal(actualSupply.toString(), tokenTotalSupply.toString());
-  });
-
-  it('has correct token name after construction', async () => {
-    const actualName = await token.name();
-    assert.equal(actualName, tokenName);
-  });
-
-  it('has correct token symbol after construction', async () => {
-    const actualSymbol = await token.symbol();
-    assert.equal(actualSymbol, tokenSymbol);
-  });
-
-  it('has correct token decimals after construction', async () => {
-    const actualDecimals = await token.decimals();
-    assert.equal(actualDecimals.toString(), tokenDecimals);
-  });
-
-  it('has correct owner token balance after construction', async () => {
-    const actualBalance = await token.balanceOf(owner);
-    assert.equal(actualBalance.toString(), ownerSupply.toString());
-  });
-
-  it('returns correct balances after transfer', async () => {
-    await token.enableTransfer();
-    await token.transfer(accounts[1], 100);
-    const firstAccountBalance = await token.balanceOf(owner);
-    const secondAccountBalance = await token.balanceOf(accounts[1]);
-    assert.equal(firstAccountBalance.toString(), tokenTotalSupply.minus(100).toString());
-    assert.equal(secondAccountBalance, 100);
-  });
-
   it('emits Transfer event on transfer', async () => {
     await token.enableTransfer();
     const { logs } = await token.transfer(accounts[1], 100);
@@ -118,26 +84,6 @@ contract('erc/Zxc', (accounts) => {
     await token.approve(accounts[1], ownerSupply.plus(1));
     await assertRevert(token.transferFrom(owner, accounts[2], ownerSupply.plus(1),
       { from: accounts[1]}));
-  });
-
-  it('returns 0 allowance by default', async () => {
-    const preApproved = await token.allowance(owner, accounts[1]);
-    assert.equal(preApproved, 0);
-  });
-
-  it('increases and decreases allowance after approval', async () => {
-    await token.approve(accounts[1], 50);
-    const postIncrease = await token.allowance(owner, accounts[1]);
-    assert.equal(postIncrease.toString(), '50');
-    await token.approve(accounts[1], 0);
-    await token.approve(accounts[1], 40);
-    const postDecrease = await token.allowance(owner, accounts[1]);
-    assert.equal(postIncrease.minus(10).toString(), postDecrease.toString());
-  });
-
-  it('throws when approving without setting it to 0 first', async () => {
-    await token.approve(accounts[1], 50);
-    await assertRevert(token.approve(accounts[1], 100));
   });
 
   it('throws when trying to transferFrom before transfers enabled', async () => {
